2017e2309dd6ec64e925e530109c129167807f7b
Cherubimbequin <cherubimbequinetornam@gmail.com>
2024-11-20 15:05:46 +0000

    should qu

--- app/Mail/PolicyPurchasedMail.php
+++ app/Mail/PolicyPurchasedMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PolicyPurchasedMail extends Mailable
+class PolicyPurchasedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
